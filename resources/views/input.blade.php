b89bc1f13983e12b1b4cf02a438ae5cdbf809923
Takuto <htakutolife@gmail.com>
2023-06-08 19:01:30 +0900

    エラーメッセージ表示

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -10,10 +10,19 @@
     <form action="{{ url('/confirm') }}" method="post">
         @csrf
         <label for="name">名前</label><br>
+        @error('name')
+            <div>{{ $message }}</div><br>
+        @enderror
         <input type="text" name="name" value="{{old('name')}}" id="name"><br>
         <label for="email">メールアドレス</label><br>
+        @error('email')
+            <div>{{ $message }}</div><br>
+        @enderror
         <input type="email" name="email" value="{{old('email')}}" id="email"><br>
         <label for="content">問い合わせ内容</label><br>
+        @error('content')
+        <div>{{ $message }}</div><br>
+        @enderror
         <textarea name="content" id="" cols="30" rows="10" id="content">{{old('content')}}</textarea><br>
         <input type="submit" value="送信" name="send">
     </form>
