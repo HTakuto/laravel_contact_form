ddb104846e2f38031033a6553fdb3b05697cb087
Takuto <htakutolife@gmail.com>
2023-06-09 20:02:57 +0900

    ユーザー登録ルーティング

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,10 @@ use App\Http\Controllers\ContactController;
 |
 */
 
+//お問い合わせ
 Route::get('/', [ContactController::class, 'index']);
 Route::post('/confirm', [ContactController::class, 'confirm']);
 Route::post('/complete', [ContactController::class, 'complete']);
+
+//管理者画面
+Route::get('/admin/index', [AdminController::class, 'index']);
