4001eca1b59f482626432436d2c00628f9deb0fb
Takuto <htakutolife@gmail.com>
2023-06-09 20:04:41 +0900

    ユーザー登録コントローラー

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,4 +12,32 @@ class AdminController extends Controller
     public function index() {
         return view('admin/index');
     }
+
+    public function register(UserRequest $request)
+    {
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $request->validate([
+            'email' => [
+                new Unique('users', 'email'),
+            ],
+        ]);
+
+        $request->session()->put('name', $name);
+        $request->session()->put('email', $email);
+        $request->session()->put('password', $password);
+
+        $user = new User();
+        $user->name = $name;
+        $user->email = $email;
+        $user->password = $password;
+        $user->save();
+
+        return redirect()->route('admin/list');
+    }
+    public function list() {
+        return view('admin/list');
+    }
 }
